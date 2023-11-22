ca0195af75909e0356c2b490c5a2215d5eafb25b
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-22 17:58:33 +0200

    Added way to create new messages

--- src/MessageDatabase.cpp
+++ src/MessageDatabase.cpp
@@ -6,14 +6,23 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:04:51 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/16 14:13:43 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/22 17:57:26 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MessageDatabase.hpp"
 #include "algorithm"
 
-void    MessageDatabase::message_remove_reference(int id)
+void    MessageDatabase::message_add(std::string const& message, unsigned int reference_count)
 {
-    std::find_if(m_messages.begin(), m_messages.end(), compare);
+    m_messages.push_back(Message(message, reference_count));
+}
+
+void    MessageDatabase::message_remove_reference(std::string const& message)
+{
+    std::vector<Message>::iterator it = std::find_if(m_messages.begin(), m_messages.end(), find_id(message));
+    if (--it->reference_count == 0)
+    {
+        m_messages.erase(it);
+    }
 }
