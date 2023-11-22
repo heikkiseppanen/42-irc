aa73a84d425c3b9243529108c131fd467b990667
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-22 17:58:33 +0200

    Added way to create new messages

--- src/MessageDatabase.hpp
+++ src/MessageDatabase.hpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 13:59:29 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/16 14:13:36 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/22 17:56:30 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,26 @@
 struct Message
 {
     std::string message;
-    int         reference_count;
-    int         id;
+    unsigned int reference_count;
+    
+    Message(std::string const& message, unsigned int reference_count) : message(message), reference_count(reference_count) {}
 };
 
 class MessageDatabase
 {
     public:
 
-        void    message_remove_reference(int id);
+        
+        void    message_add(std::string const& message, unsigned int reference_count);
+        void    message_remove_reference(std::string const& message);
     
     private:
         std::vector<Message>    m_messages;
 
-        inline bool    compare(Message message) { return message.id; }
-
+    struct find_id
+    {
+        find_id(std::string const& msg) : msg(msg) {}
+        bool operator()(Message const& message) { return message.message == msg; }
+        std::string const& msg;
+    };
 };
