be7c1722e3e7c812447585a13365806f5d621ab0
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-24 16:18:11 +0200

    Fixed crashes on destructor and copy constructor trying to derefence possible null pointer

--- src/SharedPointer.hpp
+++ src/SharedPointer.hpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 13:56:06 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/24 11:46:25 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/24 16:14:58 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ class SharedPointer
         {
             m_object = ref.m_object;
             m_ref_count = ref.m_ref_count;
-            (*m_ref_count)++;
+            if (m_ref_count)
+                (*m_ref_count)++;
         }
 
         SharedPointer& operator=(SharedPointer const& ref)
@@ -55,7 +56,8 @@ class SharedPointer
 
         ~SharedPointer()
         {
-            clear();
+            if (m_ref_count)
+                clear();
         }
 
         T const& get() const
