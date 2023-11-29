0516d83e222031763091e3c534c92f39f2ff49bd
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-29 18:51:49 +0200

    Added functionality for writeable and readable sockets

--- src/EventHandler.cpp
+++ src/EventHandler.cpp
@@ -6,23 +6,49 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:43:21 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/29 16:10:31 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/29 18:47:53 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EventHandler.hpp"
 
-void EventHandler::on_client_connected(Socket client)
+void EventHandler::on_client_connected(Socket socket)
 {
-    (void)client;
+    m_socket_client_table[socket.file_descriptor] = m_clients.add_client("User1");
 }
 
-void EventHandler::on_client_readable(Socket client)
+void EventHandler::on_client_readable(Socket socket)
 {
-    (void)client;
+    Client client = m_clients.get_client(m_socket_client_table[socket.file_descriptor]);
+
+    char buf[m_buffer_size + 1];
+    unsigned int received = socket.receive(buf, m_buffer_size);
+    buf[received] = '\0';
+
+    client.add_to_buffer(buf);
+    std::string const& buffer = client.get_buffer();
+    for (std::string::const_iterator it = buffer.begin(); it != buffer.end() - 1; it++)
+    {
+        if (*it == m_end[0] && *(it + 1) == m_end[1])
+        {
+            it += 2;
+            //TODO interpreter called here
+            client.remove_from_buffer(it - buffer.begin());
+            break;
+        }
+    }
 }
 
-void EventHandler::on_client_writeable(Socket client)
+void EventHandler::on_client_writeable(Socket socket)
 {
-    (void)client;
+    Client client = m_clients.get_client(m_socket_client_table[socket.file_descriptor]);
+    if (client.has_message())
+    {
+        std::string msg = client.get_message();
+        unsigned int sent_count = client.get_sent_count();
+        sent_count += socket.send(msg.c_str() + sent_count, msg.length() - sent_count);
+        if (sent_count == msg.length())
+            sent_count = 0;
+        client.set_sent_count(sent_count);
+    }
 }
