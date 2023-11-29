db5e37df005322da4cae68fd7a4ba771b6ea5a38
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-29 18:50:26 +0200

    Added buffer for reading from clients

--- src/Client.cpp
+++ src/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:54:18 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/29 13:42:38 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/29 16:24:00 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void Client::print_messages() const
 {
-    for (std::vector<SharedPointer<std::string> >::const_iterator msg = message_queue.begin(); msg != message_queue.end(); msg++)
+    for (std::vector<SharedPointer<std::string> >::const_iterator msg = m_message_queue.begin(); msg != m_message_queue.end(); msg++)
     {
         std::cout << **msg << " | ";
     }
