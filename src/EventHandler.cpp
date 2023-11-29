1b784f3cdf3c86f0383df03cdef5c9cadae1ba06
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-29 16:14:26 +0200

    Fixed compilation

--- src/EventHandler.cpp
+++ src/EventHandler.cpp
@@ -6,9 +6,23 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:43:21 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/29 12:43:28 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/29 16:10:31 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EventHandler.hpp"
 
+void EventHandler::on_client_connected(Socket client)
+{
+    (void)client;
+}
+
+void EventHandler::on_client_readable(Socket client)
+{
+    (void)client;
+}
+
+void EventHandler::on_client_writeable(Socket client)
+{
+    (void)client;
+}
