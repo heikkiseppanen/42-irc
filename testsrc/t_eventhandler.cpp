d99d0ad3824948bdeceaff109b8aedc07f398b32
Eemil Majuri <eemilmajuri@gmail.com>
2024-01-15 17:51:48 +0200

    More tests

--- testsrc/t_eventhandler.cpp
+++ testsrc/t_eventhandler.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 16:58:55 by emajuri           #+#    #+#             */
-/*   Updated: 2024/01/15 19:18:56 by emajuri          ###   ########.fr       */
+/*   Updated: 2024/01/15 19:20:40 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,6 @@ void test1_handler()
     c.add_to_buffer("hello world!\r\n");
     if (!find_command(cmd, c))
         TEST_ERROR("Find command not finding command");
-    if (cmd != "hello world!")
-        TEST_ERROR("Incorrect command found");
-    if (c.get_buffer().length() != 0)
-        TEST_ERROR("Erased incorrectly");
-    if (find_command(cmd, c))
-        TEST_ERROR("Finding on empty buffer");
-
-
-    c.add_to_buffer("hello world!");
-    if (find_command(cmd, c))
-        TEST_ERROR("Finding command when there is no end");
-    c.add_to_buffer("\r");
-    if (find_command(cmd, c))
-        TEST_ERROR("Finding command when there is no end");
-    c.add_to_buffer("\n");
-    if (!find_command(cmd, c))
-        TEST_ERROR("not finding command");
     if (cmd != "hello world!")
         TEST_ERROR("Incorrect command found");
     if (c.get_buffer().length() != 0)
