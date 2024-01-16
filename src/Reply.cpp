7e058301e9a80ef61b227887296350ffe90facd8
Eemil Majuri <eemilmajuri@gmail.com>
2024-01-16 17:22:07 +0200

    Added correct enum values for enums

--- src/Reply.cpp
+++ src/Reply.cpp
@@ -6,16 +6,51 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:21:52 by emajuri           #+#    #+#             */
-/*   Updated: 2024/01/12 14:21:25 by emajuri          ###   ########.fr       */
+/*   Updated: 2024/01/16 17:20:34 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Reply.hpp"
 
-Reply::Reply(ClientDatabase& clients, ChannelDatabase& channels) : m_clients(clients), m_channels(channels)
-{
-    m_replies[RPL_WELCOME] = "Welcome to the Internet Relay Network <nick>!<user>@<host>";
-}
+//all of these come with rpl welcome
+// :<hostname> <enum> <target> :<MSG> <hostname> <MSG> <ver> // rpl_yourhost
+// :<hostname> <enum> <target> :<MSG> <date> // rpl_created
+// :<hostname> <enum> <target> :<hostname> <ver> <usermodes> <channelmodes> // rpl_myinfo
+// :<hostname> <enum> <target> :<MSG> <integer> <MSG> <integer> <MSG> <integer> <MSG> // RPL_LUSERCLIENT
+// :<hostname> <enum> <target> <integer :<MSG> // RPL_LUSEROP
+// :<hostname> <enum> <target> <integer :<MSG> // RPL_LUSERUNKNOWN
+// :<hostname> <enum> <target> <integer :<MSG> // RPL_LUSERCHANNELS
+// :<hostname> <enum> <target> :<MSG> <integer> <MSG> <integer> <MSG> // RPL_LUSERME
+
+
+// :<hostname> <enum> <target> :<MSG> <target> // rpl_welcome 1
+// :<hostname> <enum> <target> <channel> <mode> <mode params> // rpl_channelmodeis 324
+// :<hostname> <enum> <target> <channel> :<MSG> // rpl_notopic 331
+// :<hostname> <enum> <target> <channel> :<topic> // rpl_topic 332
+// :<hostname> <enum> <target> <channel> <nick> // rpl_inviting 341
+// :<hostname> <enum> <target> <channel> <invitemask> // rpl_invitelist 346
+// :<hostname> <enum> <target> <channel> :<MSG> // rpl_endofinvitelist 347
+// :<hostname> <enum> <target> "( "=" / "*" / "@" ) <channel> :[ "@" / "+" ] <nick> *( " " [ "@" / "+" ] <nick> ) // rpl_namreply 353
+
+
+// :<hostname> <enum> <target> <nick> :<MSG> // err_nosuchnick 401
+// :<hostname> <enum> <target> <channel> :<MSG> // err_nosuchchannel 403
+// :<hostname> <enum> <target> :<MSG> // err_notexttosend 412
+// :<hostname> <enum> <target> :<MSG> // err_nonicknamegiven 431
+// :<hostname> <enum> <target> <nick> :<MSG> // err_erroneusnickname 432
+// :<hostname> <enum> <target> <nick> :<MSG> // err_nicknameinuse 433
+// :<hostname> <enum> <target> <nick> <channel> :<MSG> // err_usernotinchannel 441
+// :<hostname> <enum> <target> <channel> :<MSG> // err_notonchannel 442
+// :<hostname> <enum> <target> <nick> <channel> :<MSG> // err_useronchannel 443
+// :<hostname> <enum> <target> <command> :<MSG> // err_needmoreparams 461
+// :<hostname> <enum> <target> :<MSG> // err_alreadyregistred 462
+// :<hostname> <enum> <target> <channel> :<MSG> // err_keyset 467
+// :<hostname> <enum> <target> <channel> :<MSG> // err_channelisfull 471
+// :<hostname> <enum> <target> <char> :<MSG> <channel> // err_unknownmode 472
+// :<hostname> <enum> <target> <channel> :<MSG> // err_inviteonlychan 473
+// :<hostname> <enum> <target> <channel> :<MSG> // err_badchannelkey 475
+// :<hostname> <enum> <target> <channel> :<MSG> // err_chanoprivsneeded 482
+
 
 void Reply::reply_to_sender(unsigned int user_id, ReplyEnum reply)
 {
@@ -23,29 +58,7 @@ void Reply::reply_to_sender(unsigned int user_id, ReplyEnum reply)
     switch (reply)
     {
         case RPL_WELCOME:
-            client.add_message(std::make_shared<std::string>("Welcome to the Internet Relay Network " + client.get_nickname() + "!" + "<user>" + "@" + "<host>")); // TODO <user> and <host>
-        default:
-            (void)1;
-    }
-
-}
-
-void Reply::error_to_sender(unsigned int user_id, ReplyEnum reply, std::string filler1, std::string filler2)
-{
-    (void)filler2;
-    Client client = m_clients.get_client(user_id);
-    switch (reply)
-    {
-        case ERR_NEEDMOREPARAMS:
-            client.add_message(std::make_shared<std::string>(filler1 + " :Not enough parameters"));
-        case ERR_ALREADYREGISTRED:
-            client.add_message(std::make_shared<std::string>(":Unauthorized command (already registered)"));
-        case ERR_NONICKNAMEGIVEN:
-            client.add_message(std::make_shared<std::string>(":No nickname given"));
-        case ERR_ERRONEUSNICKNAME:
-            client.add_message(std::make_shared<std::string>(filler1 + " :Erroneous nickname"));
-        case ERR_NICKNAMEINUSE:
-            client.add_message(std::make_shared<std::string>(filler1 + " :Nickname is already in use"));
+            client.add_message(std::make_shared<std::string>("Welcome to the Internet Relay Network " + client.get_nickname()));
         default:
             (void)1;
     }
--- src/Reply.hpp
+++ src/Reply.hpp
@@ -22,41 +22,35 @@ class ChannelDatabase;
 enum ReplyEnum
 {
     IGNORE, // Incase no reply is meant to be sent
-    RPL_WELCOME, // "Welcome to the Internet Relay Network <nick>!<user>@<host>"
-    RPL_TOPIC, // "<channel> :<topic>" 
-    RPL_NOTOPIC, // "<channel> :No topic is set"
-    RPL_CHANNELMODEIS, // "<channel> <mode> <mode params>"
-    RPL_INVITING, //"<channel> <nick>"
-    RPL_NAMREPLY, //"( "=" / "*" / "@" ) <channel>
+    RPL_WELCOME = 1, // "Welcome to the Internet Relay Network <nick>!<user>@<host>"
+    RPL_CHANNELMODEIS = 324, // "<channel> <mode> <mode params>"
+    RPL_NOTOPIC = 331, // "<channel> :No topic is set"
+    RPL_TOPIC = 332, // "<channel> :<topic>" 
+    RPL_INVITING = 341, //"<channel> <nick>"
+    RPL_INVITELIST = 346, // "<channel> <invitemask>"
+    RPL_ENDOFINVITELIST = 347, // "<channel> :End of channel invite list"
+    RPL_NAMREPLY = 353, //"( "=" / "*" / "@" ) <channel>
         //        :[ "@" / "+" ] <nick> *( " " [ "@" / "+" ] <nick> )
         //  - "@" is used for secret channels, "*" for private
         //    channels, and "=" for others (public channels).
 
-    RPL_INVITELIST, // "<channel> <invitemask>"
-    RPL_ENDOFINVITELIST, // "<channel> :End of channel invite list"
-
-    ERR_NEEDMOREPARAMS, // "<command> :Not enough parameters"
-    ERR_NOSUCHCHANNEL, // "<channel> :No such channel" 
-    ERR_CHANNELISFULL, //  "<channel> :Cannot join channel (+l)"
-    ERR_INVITEONLYCHAN, // "<channel> :Cannot join channel (+i)"
-    ERR_BADCHANNELKEY, // "<channel> :Cannot join channel (+k)"
-    ERR_NONICKNAMEGIVEN, // ":No nickname given"
-    ERR_ERRONEUSNICKNAME, // "<nick> :Erroneous nickname"
-    ERR_NICKNAMEINUSE, // "<nick> :Nickname is already in use"
-    ERR_ALREADYREGISTRED, //":Unauthorized command (already registered)"
-    ERR_NOTONCHANNEL, // "<channel> :You're not on that channel"
-    ERR_CHANOPRIVSNEEDED, // "<channel> :You're not channel operator"
-    ERR_USERNOTINCHANNEL, // "<nick> <channel> :They aren't on that channel"
-    ERR_NOSUCHNICK, // "<nickname> :No such nick/channel"
-    ERR_USERONCHANNEL, // "<user> <channel> :is already on channel"
-    ERR_NOTEXTTOSEND, //":No text to send"
-
-    // ERR_NOCHANMODES, // "<channel> :Channel doesn't support modes"
-    ERR_KEYSET, // "<channel> :Channel key already set"
-    ERR_UNKNOWNMODE, // "<char> :is unknown mode char to me for <channel>"
-    // ERR_NOORIGIN, // ":No origin specified"
-    // ERR_NOSUCHSERVER // "<server name> :No such server"
-    ERR_UNKNOWNCOMMAND //"<command> :Unknown command"
+    ERR_NOSUCHNICK = 401, // "<nickname> :No such nick/channel"
+    ERR_NOSUCHCHANNEL = 403, // "<channel> :No such channel" 
+    ERR_NOTEXTTOSEND = 412, //":No text to send"
+    ERR_NONICKNAMEGIVEN = 431, // ":No nickname given"
+    ERR_ERRONEUSNICKNAME = 432, // "<nick> :Erroneous nickname"
+    ERR_NICKNAMEINUSE = 433, // "<nick> :Nickname is already in use"
+    ERR_USERNOTINCHANNEL = 441, // "<nick> <channel> :They aren't on that channel"
+    ERR_NOTONCHANNEL = 442, // "<channel> :You're not on that channel"
+    ERR_USERONCHANNEL = 443, // "<user> <channel> :is already on channel"
+    ERR_NEEDMOREPARAMS = 461, // "<command> :Not enough parameters"
+    ERR_ALREADYREGISTRED = 462, //":Unauthorized command (already registered)"
+    ERR_KEYSET = 467, // "<channel> :Channel key already set"
+    ERR_CHANNELISFULL = 471, //  "<channel> :Cannot join channel (+l)"
+    ERR_UNKNOWNMODE = 472, // "<char> :is unknown mode char to me for <channel>"
+    ERR_INVITEONLYCHAN = 473, // "<channel> :Cannot join channel (+i)"
+    ERR_BADCHANNELKEY = 475, // "<channel> :Cannot join channel (+k)"
+    ERR_CHANOPRIVSNEEDED = 482, // "<channel> :You're not channel operator"
 };
 
 class Reply
@@ -64,8 +58,7 @@ class Reply
     public:
 
         Reply() = delete;
-        // Reply(ClientDatabase& clients);
-        Reply(ClientDatabase& clients, ChannelDatabase& channels);
+        Reply(ClientDatabase& clients, ChannelDatabase& channels) : m_clients(clients), m_channels(channels) {}
 
         void reply_to_sender(unsigned int user_id, ReplyEnum reply);
         void error_to_sender(unsigned int user_id, ReplyEnum error, std::string filler1, std::string filler2);
@@ -76,5 +69,4 @@ class Reply
 
         ClientDatabase& m_clients;
         ChannelDatabase& m_channels;
-        std::map<ReplyEnum, std::string> m_replies;
 };
