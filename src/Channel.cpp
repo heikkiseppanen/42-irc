064108f971815d342129196e6fb4cc584fa06ce7
Jonni Le <jole@student.hive.fi>
2024-01-31 18:35:54 +0200

    Changed set_op to return void

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: jole <jole@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:11:27 by emajuri           #+#    #+#             */
-/*   Updated: 2024/01/31 16:32:38 by jole             ###   ########.fr       */
+/*   Updated: 2024/01/31 18:36:41 by jole             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,24 +123,14 @@ ReplyEnum Channel::set_password(unsigned int op_id, bool mode, std::string const
     return RPL_CHANNELMODEIS;
 }
 
-ReplyEnum Channel::set_op(unsigned int op_id, bool mode, unsigned int affect_id)
+void Channel::set_op(bool mode, unsigned int affect_id)
 {
-    if (!is_operator(op_id))
-        return ERR_CHANOPRIVSNEEDED;
-
-    if (!is_subscribed(affect_id))
-        return ERR_USERNOTINCHANNEL;
-
     std::vector<unsigned int>::iterator it = std::find(m_operators.begin(), m_operators.end(), affect_id);
     if (mode == ADD)
     {
         if (it == m_operators.end())
         {
             m_operators.push_back(affect_id);
-        }
-        else
-        {
-            return IGNORE;
         }
     }
     else
@@ -149,12 +139,7 @@ ReplyEnum Channel::set_op(unsigned int op_id, bool mode, unsigned int affect_id)
         {
             m_operators.erase(it);
         }
-        else
-        {
-            return IGNORE;
-        }
     }
-    return RPL_NAMREPLY;
 }
 
 ReplyEnum Channel::set_user_limit(unsigned int op_id, bool mode, unsigned int user_limit)
