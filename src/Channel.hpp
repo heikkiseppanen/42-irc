9a032c1c338b85712544299222b7cf176b6fcc81
Jonni Le <jole@student.hive.fi>
2024-01-31 16:07:12 +0200

    Added a bunch of methods to check for current-modes

--- src/Channel.hpp
+++ src/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: jole <jole@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:51:27 by emajuri           #+#    #+#             */
-/*   Updated: 2024/01/26 17:36:24 by emajuri          ###   ########.fr       */
+/*   Updated: 2024/01/31 16:32:11 by jole             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,9 @@ class Channel
         bool is_not_full() const;
         bool is_subscribed(unsigned int user_id) const;
         inline bool is_channel_topic_empty() const { return m_topic.empty(); }
-        inline bool is_topic_op_mode_on() const { return m_has_op_topic; }
+        inline bool is_invite_only() const { return m_has_invite_only; }
+        inline bool is_topic_op_only() const { return m_has_op_topic; }
+        inline bool has_password() const { return m_has_password; }
 
     private: 
 
